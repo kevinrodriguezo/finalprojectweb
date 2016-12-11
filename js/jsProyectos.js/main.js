3e6378eacf4074b4c60e5150f074f7739fd01922
kevinrodriguezo <kevin.1296@hotmail.com>
2016-12-10 21:49:42 -0600

    Avances

---
+++ js/jsProyectos.js/main.js
@@ -0,0 +1,12 @@
+$(document).jQuery(document).ready(function($) {
+
+var animales = [];
+function load_data() 
+{
+	debugger;
+	localStorage.setItem("NombrePro", document.NombreProyecto.value);
+	var objetoSelects = {"items": x, "select": itemx}
+}
+	
+});
+
