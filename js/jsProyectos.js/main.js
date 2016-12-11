a2146dddeb0409e554b5cc8f6a39ef9bd5db9c40
kevinrodriguezo <kevin.1296@hotmail.com>
2016-12-10 22:58:05 -0600

    Midific

--- js/jsProyectos.js/main.js
+++ js/jsProyectos.js/main.js
@@ -1,12 +1,27 @@
-$(document).jQuery(document).ready(function($) {
+var proyectos = [];
 
-var animales = [];
-function load_data() 
-{
-	debugger;
-	localStorage.setItem("NombrePro", document.NombreProyecto.value);
-	var objetoSelects = {"items": x, "select": itemx}
+var Persister = {
+	save: function (key, value) {
+		localStorage.setItem(key, value);
+	},
+	load: function(key, default_value) {
+		return localStorage.getItem(key) || default_value;
+	},
+	saveObj: function(key, value) {
+		var json_string = JSON.stringify(value);
+		this.save(key, json_string);
+	},
+	loadObj: function(key, default_value) {
+		var json_string = this.load(key, default_value);
+		return JSON.parse(json_string);
+	}
+};
+
+function load_data() {
+	proyectos = Persister.loadObj('proyectos', "[]");
+	$('#proyectos').html('');
+	for (var i = 0; i < animales.length; i++) {
+		$('#animales').append('<li>' + animales[i].nombre + '</li>');
+	}
 }
-	
-});
 
